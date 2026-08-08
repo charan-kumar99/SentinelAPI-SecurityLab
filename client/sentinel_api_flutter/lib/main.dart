18d5b7376ca90479645eea61a3b5d1476f17bd38
Charan Kumar <charansuvarna99@gmail.com>
2026-08-08 11:09:46 +0530

    revert(footer): remove custom creator footer and clean dependencies

--- client/sentinel_api_flutter/lib/main.dart
+++ client/sentinel_api_flutter/lib/main.dart
@@ -9,7 +9,6 @@ import 'views/sandbox_view.dart';
 import 'views/crypto_view.dart';
 import 'views/rate_limit_view.dart';
 import 'views/audit_view.dart';
-import 'widgets/cyber_footer.dart';
 
 void main() {
   runApp(const SentinelApiApp());
@@ -177,9 +176,6 @@ class _MainDashboardScreenState extends State<MainDashboardScreen>
                 ],
               ),
             ),
-
-            // Bottom DevSecOps Cyber Footer
-            const CyberFooter(),
           ],
         ),
       ),
